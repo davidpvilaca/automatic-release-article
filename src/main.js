593672f55711710b7fcebf734cb70f9cb024f02c
David Vilaça <vilacapdavid@gmail.com>
2020-03-23 08:31:46 -0300

    feat: 🎸 limit ping route to GET method only
    BREAKING CHANGE: 🧨 remove /ping POST, PUT and DELETE

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ const pkg = require('../package.json');
 
 const app = express();
 
-app.all('/ping', (req, res) => res.json('pong'));
+app.get('/ping', (req, res) => res.json('pong'));
 app.get('/time', (req, res) => res.json({ now: new Date() }));
 app.get('/version', (req, res) => res.json({ version: pkg.version }));
+
+// Error handling
+app.use((req, res, next) => res.status(404).json({ message: 'Sorry cant find that!' }));
 
 module.exports = app;
